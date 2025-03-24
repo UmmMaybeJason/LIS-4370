544ffe8fe727a1cbc2bdf00027ed9fd0e181abc9
NotJason <46411801+UmmMaybeJason@users.noreply.github.com>
2025-03-24 04:23:01 -0400

    Module 9.r

--- Module 9.r
+++ Module 9.r
@@ -13,3 +13,16 @@ plot(cigarette_data$packs, cigarette_data$price,
      xlab = "Average Price", 
      ylab = "Average Consumption", 
      col = "blue")
+
+xyplot(price ~ packs | rownames, data = cigarette_data,
+       main = "Price vs. Cigarette Consumption by State",
+       xlab = "Average Consumption (packs)",
+       ylab = "Average Price",
+       col = "blue")
+
+ggplot(cigarette_data, aes(x = cigarette_data$packs, y = cigarette_data$price)) +
+  geom_point(color = "blue") +
+  geom_smooth(method = "lm", col = "red") +
+  labs(title = "Consumption vs. Price",
+       x = "Average Price",
+       y = "Average Consumption")
